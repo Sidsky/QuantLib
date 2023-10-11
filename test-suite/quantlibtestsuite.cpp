195f78f49cd5a37e312b27ffaf4bd1d3ce047eb0
siddharthmehrotra <siddharthmehrotra1234@gmail.com>
2023-10-11 15:36:31 +0800

    removed unused file

--- test-suite/quantlibtestsuite.cpp
+++ test-suite/quantlibtestsuite.cpp
@@ -477,7 +477,6 @@ test_suite* init_unit_test_suite(int, char* []) {
     test->add(UltimateForwardTermStructureTest::suite());
     test->add(VarianceSwapTest::suite());
     test->add(VolatilityModelsTest::suite());
-    test->add(Xoshiro256StarStarTest::suite());
     test->add(ZeroCouponSwapTest::suite());
 
     // tests for experimental classes
